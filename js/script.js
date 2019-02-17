19428db6f4214fc09b31f2a1b764b5e691806ff3
JSMaOm <marah937aboud@gmail.com>
2019-02-17 10:48:29 +0800

    upload the files

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const backFacesArray = ['😲', '😴', '😑', '😍', '😜', '😒', '😁', '😉'];
+const frontFaces = document.querySelectorAll('.js_front'),
+  backFaces = document.querySelectorAll('.js_back'),
+  idexs = [],
+  duplicatArray = duplicat(backFacesArray);
+
+function duplicat(array) {
+  const dArray = [];
+  array.map(item => {
+    dArray.push(item);
+    dArray.push(item);
+  });
+  return dArray;
+}
+
+function randomIdx() {
+  if (idexs.length === duplicatArray.length) return;
+  const idx = Math.floor(Math.random() * duplicatArray.length);
+  if (!idexs.includes(idx)) {
+    idexs.push(idx);
+    randomIdx();
+  } else randomIdx();
+}
+
+function pasteFace() {
+  randomIdx()
+  let i = 0;
+  backFaces.forEach(back => {
+    back.textContent = duplicatArray[idexs[i++]];
+  })
+}
+
+pasteFace();
+
